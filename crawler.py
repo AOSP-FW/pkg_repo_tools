21bea50a0f98961044b11e34bc73112c88600658
skyleaworlder <870033938@qq.com>
2021-12-18 17:48:00 +0800

    update: 修改输出，去掉前缀 platform/

--- crawler.py
+++ crawler.py
@@ -3,4 +3,4 @@ from pkg_repo_tools.services.repo_link import RepoLinkService
 with open("framework.repo.list", "w") as f:
     repo_link_list = RepoLinkService.getRepoLinkListByPrefix("platform/frameworks")
     for link in repo_link_list:
-        f.write(link.name + "\n")
+        f.write(link.name[len("platform/"):] + "\n")
